b3d029644b3b7ddef925af1666e244689cb91b19
jorgeGuevara <jorge.guevara@student.ehb.be>
2020-12-10 13:04:51 +0100

    feat: create tables

--- api/src/utils/DatabaseHelper.js
+++ api/src/utils/DatabaseHelper.js
@@ -1,3 +1,5 @@
+const Helpers = require('./helpers');
+
 const pg = require('knex')({
   client: 'pg',
   version: '9.6',
@@ -8,6 +10,50 @@ const pg = require('knex')({
 });
 
 async function initialiseTables() {
+  await pg.schema.hasTable('emotions').then(async (exists) => {
+    if (!exists) {
+      await pg.schema
+        .createTable('emotions', (table) => {
+          table.increments();
+          table.uuid('uuid');
+          table.string('emotion');
+          table.string('category_id');
+          table.timestamps(true, true);
+        })
+        .then(async () => {
+          console.log('created table EMOTIONS');
+        });
+    }
+    // add record to storyblock table
+    else {
+      // console.log('created table STORYBLOCK');
+      // const uuid = Helpers.generateUUID();
+      // await pg.table('storyblock').insert({ uuid, content: `add record` });
+    }
+  });
+  await pg.schema.hasTable('e_categories').then(async (exists) => {
+    if (!exists) {
+      await pg.schema
+        .createTable('e_categories', (table) => {
+          table.increments();
+          table.uuid('uuid');
+          table.string('category');
+          table.timestamps(true, true);
+        })
+        .then(async () => {
+          console.log('created table CATEGORIES');
+          for (let i = 0; i < 3; i++) {
+            const uuid = Helpers.generateUUID();
+            await pg
+              .table('e_categories')
+              .insert({ uuid, category: `category ${i}` });
+          }
+        });
+    }
+  });
+
+  ////STORY tables -- to be refactored
+
   await pg.schema.hasTable('storyblock').then(async (exists) => {
     if (!exists) {
       await pg.schema
